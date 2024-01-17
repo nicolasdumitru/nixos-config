a01d35474b935be9563510be98d5566ac7a8e26f
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-01-18 00:27:30 +0200

    Pipewire

--- hosts/hermes/default.nix
+++ hosts/hermes/default.nix
@@ -103,6 +103,15 @@
   # Enable slock
   programs.slock.enable = true;
 
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
