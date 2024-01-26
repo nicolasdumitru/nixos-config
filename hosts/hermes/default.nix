e8faa89b908b6d84db55a4f76b064b194eeba326
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-01-26 21:39:04 +0200

    packages

--- hosts/hermes/default.nix
+++ hosts/hermes/default.nix
@@ -139,21 +139,34 @@
   environment.systemPackages = with pkgs; [
     zsh
 
-    picom
     dmenu
 
     alacritty
     librewolf
     neovim
+    emacs
+    newsboat
+    fzf
 
     ripgrep
     fd
     git
     curl
     wget
+    pstree
+	rsync
+    bat
+	lsof
 
-    xorg.xmodmap
     lightdm
+    picom
+    xorg.xmodmap
+	arandr
+    xclip
+    xsel
+	maim
+	xcolor
+    feh
 
     gcc
     clang
@@ -166,14 +179,16 @@
 
     clang-tools
     lua-language-server
+	# bash-language-server
+	shellcheck
     nil
     nixfmt
 
+	gdb
+
+	# direnv
+
     networkmanagerapplet
-    feh
-    fzf
-    emacs
-    newsboat
 
     dunst
     libnotify
@@ -183,15 +198,25 @@
     btop
     #pinentry
     gnupg
-    lshw
     xdotool
-    xclip
-    xsel
     pulseaudio
     pulsemixer
     sqlite
-    bat
-    pstree
+	syncthing
+	mpd
+	ncmpcpp
+	mpv
+	nsxiv
+	zathura
+	isync
+	msmtp
+
+    lshw
+	usbutils
+	exfat
+	exfatprogs
+
+	plan9port
   ];
 
   fonts.packages = with pkgs; [
