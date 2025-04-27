d0eb4fec8dfae0810e6b73f8f736ad1ba3d9e4d2
nicolasdumitru <nicolas@ndumitru.com>
2025-04-27 22:55:51 +0300

    pkgs(development/python): Add pandas

--- modules/development/python.nix
+++ modules/development/python.nix
@@ -7,8 +7,10 @@
   environment.systemPackages = with pkgs; [
     (python3.withPackages (
       ps: with ps; [
+        # Libraries
         numpy
         scipy
+        pandas
         torchWithCuda
         matplotlib
 
