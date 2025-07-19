03f86c6d1f8a5b8b0a9e548081a61e2a4120b440
nicolasdumitru <nicolas@ndumitru.com>
2025-07-19 18:18:55 +0300

    Remove Lix

--- flake.nix
+++ flake.nix
@@ -6,12 +6,6 @@
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Lix
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     # disko
     disko-stable.url = "github:nix-community/disko";
     disko-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
@@ -30,7 +24,6 @@
       self,
       nixpkgs,
       nixpkgs-stable,
-      lix-module,
       disko-stable,
       disko-unstable,
       rust-overlay,
@@ -55,8 +48,6 @@
 
           modules = [
             ./hosts/turing
-
-            lix-module.nixosModules.default
 
             disko-unstable.nixosModules.disko
 
@@ -88,8 +79,6 @@
 
           modules = [
             ./hosts/hermes
-
-            lix-module.nixosModules.default
 
             disko-unstable.nixosModules.disko
 
