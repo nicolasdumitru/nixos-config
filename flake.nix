3e1322c81f4d3e8475c64acab26b124719c59371
nicolasdumitru <nicolas@ndumitru.com>
2025-11-24 00:17:36 +0200

    flake(turing): Enable system-wide binary caching for CUDA packages

--- flake.nix
+++ flake.nix
@@ -83,12 +83,12 @@
                 # TODO: The default is preserved, but make appending/prepending
                 #       to the lists (substituters & trusted keys) explicit
                 substituters = [
+                  "https://cache.nixos-cuda.org"
                   "https://nix-community.cachix.org"
-                  # "https://cuda-maintainers.cachix.org/"
                 ];
                 trusted-public-keys = [
+                  "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
                   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                  # "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
                 ];
               };
             }
