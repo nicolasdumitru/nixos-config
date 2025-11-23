80d4749d9782b2160f6c811d967d04ad65584f75
nicolasdumitru <nicolas@ndumitru.com>
2025-11-24 00:21:48 +0200

    virtualization: Enable Docker

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -12,6 +12,12 @@
     "kvm.enable_virt_at_load=0"
   ];
 
+  # Docker
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+  };
+
   # VirtualBox
   virtualisation.virtualbox.host.enable = true;
   # virtualisation.virtualbox.host.enableExtensionPack = true;
