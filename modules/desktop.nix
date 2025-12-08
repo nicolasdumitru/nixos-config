033ad383eee9a6284639f733babe84e95d922a8f
nicolasdumitru <nicolas@ndumitru.com>
2025-12-08 12:11:46 +0200

    desktop: Exclude kate from KDE Plasma packages

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -8,6 +8,9 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    kate
+  ];
 
   # COSMIC Desktop
   # services.desktopManager.cosmic.enable = true;
