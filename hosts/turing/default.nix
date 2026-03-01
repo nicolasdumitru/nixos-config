0145082ca9f22cf7cfa80c20bd90b0347639cc11
nicolasdumitru <nicolas@ndumitru.com>
2026-03-01 12:52:18 +0200

    turing: Temporarily disable avahi

--- hosts/turing/default.nix
+++ hosts/turing/default.nix
@@ -86,16 +86,16 @@ in
 
   # Avahi enables mDNS resolution, allowing SSH connections using .local
   # hostnames instead of IP addresses.
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    nssmdns6 = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      workstation = true;
-    };
-  };
+  # services.avahi = {
+  #   enable = false;
+  #   nssmdns4 = true;
+  #   nssmdns6 = true;
+  #   publish = {
+  #     enable = true;
+  #     addresses = true;
+  #     workstation = true;
+  #   };
+  # };
 
   # Set your time zone.
   time.timeZone = "Europe/Bucharest";
